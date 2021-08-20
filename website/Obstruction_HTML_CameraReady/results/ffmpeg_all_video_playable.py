d366f1e732792b23f7592638a77bc998825e0756
Yu-Lun Liu <yulunliu@cmlab.csie.ntu.edu.tw>
2021-08-20 10:48:30 +0800

    Delete ffmpeg_all_video_playable.py

--- website/Obstruction_HTML_CameraReady/results/ffmpeg_all_video_playable.py
+++
@@ -1,8 +0,0 @@
-import os
-import glob
-
-all_mp4 = sorted(glob.glob('video_full_reflection/**/*_Liu2020.mp4'))
-
-for mp4 in all_mp4:
-    print(mp4)
-    os.system('ffmpeg -y -i "'+mp4+'" "'+mp4[:-4]+'_ffmpeg.mp4"')
